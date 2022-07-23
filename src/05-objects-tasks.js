48affd16af8c231728a0f651c0639ae1cc1cbfe4
Мария В. Васильева <VasilyevaMV@PRANA.COM>
2022-07-23 19:21:59 +0400

    feat: add solutions to obj tasks (except last task)

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const res = {};
+  res.width = width;
+  res.height = height;
+  res.getArea = function () {
+    return this.width * this.height;
+  };
+  return res;
 }
 
 
@@ -35,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +57,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.assign(Object.create(proto), JSON.parse(json));
 }
 
 
@@ -111,31 +117,31 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
+  element(value) {
     throw new Error('Not implemented');
   },
 
-  id(/* value */) {
+  id(value) {
     throw new Error('Not implemented');
   },
 
-  class(/* value */) {
+  class(value) {
     throw new Error('Not implemented');
   },
 
-  attr(/* value */) {
+  attr(value) {
     throw new Error('Not implemented');
   },
 
-  pseudoClass(/* value */) {
+  pseudoClass(value) {
     throw new Error('Not implemented');
   },
 
-  pseudoElement(/* value */) {
+  pseudoElement(value) {
     throw new Error('Not implemented');
   },
 
-  combine(/* selector1, combinator, selector2 */) {
+  combine(selector1, combinator, selector2) {
     throw new Error('Not implemented');
   },
 };
